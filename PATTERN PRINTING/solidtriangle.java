a399b0ff782bae909e36f5da5314bb26f702f796
jaygautam13 <gautamjay1304@gmail.com>
2026-03-04 23:45:04 +0530

    pattern

--- PATTERN PRINTING/solidtriangle.java
+++ PATTERN PRINTING/solidtriangle.java
@@ -6,7 +6,7 @@ public class solidtriangle {
     int n = sc.nextInt() ;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i ; j++) {
-            System.out.print("*");
+            System.out.print("*git");
         }
         System.out.println();
     }
