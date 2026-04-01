31d6671d5c461ac90aaab48cc7fadd8f33b27319
jaygautam13 <jaygautamlu13@gmail.com>
2026-04-01 21:30:18 +0530

    updated

--- IF_ELSE/absolutevalue.java
+++ IF_ELSE/absolutevalue.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class absolutevalue {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt() ; 
+       int n = sc.nextInt()  ; 
        if (n<0) {
         n= n * (-1) ;
         
