f230c61ffcc198b87287cf1b4c5dae5065065f35
jaygautam13 <jaygautamlu13@gmail.com>
2026-04-02 23:53:22 +0530

    practise question

---
+++ PRACTISE QUESTION/palindome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class palindome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+        int original = n ;
+        int rev = 0 ;
+        while (n!=0) {
+          int lastdigit = n % 10 ;
+            rev = rev * 10 + lastdigit ; 
+           n = n / 10 ; 
+        }
+        if(original == rev)
+        {
+            System.out.println("the number is palindrome");
+        }
+    }
+}
